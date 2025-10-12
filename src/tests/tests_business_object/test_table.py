c6e522e27480685052c5c337d5e50af9a55770a9
marinepac123 <marinepachy@gmail.com>
2025-10-12 16:37:43 +0000

    suite table et test table : il ne reste que lancer_manche que je comprend pas

--- src/tests/tests_business_object/test_table.py
+++ src/tests/tests_business_object/test_table.py
@@ -31,6 +31,16 @@ class Test:
         assert len(table) == len(table_attendu)
         assert table.joueurs == table_attendu.joueurs
 
+    def test_table_ajouter_joueur_type_incorrecte(self):
+        # GIVEN
+        joueur = (1, "alice", 1000, True, "France", 20)
+        table = Table(1,10,1,[])
+        message_attendu = "Le joueur n'est pas une instance de joueur"
+
+        # WHEN / THEN
+        with pytest.raises(TypeError, match=message_attendu):
+            table.ajouter_joueur(joueur)
+
     def test_table_ajouter_joueur_valeur_incorrecte(self):
         # GIVEN
         joueur = Joueur(1, "alice", 1000, True, "France", 20)
@@ -43,22 +53,23 @@ class Test:
 
     def test_table_retirer_joueur_succes(self):
         # GIVEN
-        table_attendu = Table(10,10,1,[])
         joueur = Joueur(1, "alice", 1000, True, "France", 20)
         table = Table(10,10,1,[joueur])
+        table_attendu = Table(10,10,1,[])
         indice = 0
 
         # WHEN
-        table.retirer_joueur(indice)
+        joueur_supprimer = table.retirer_joueur(indice)
 
         # THEN
         assert len(table) == len(table_attendu)
         assert table.joueurs == table_attendu.joueurs
+        assert joueur_supprimer == joueur
 
     def test_table_retirer_joueur_valeur_incorrecte(self):
         # GIVEN
         table = Table(1,10,1,[])
-        message_attendu = "Indice de joueur incorrect"
+        message_attendu = "Indice négatif impossible"
         indice = -6
 
         # WHEN / THEN
@@ -68,27 +79,44 @@ class Test:
     def test_table_retirer_joueur_valeur_incorrecte2(self):
         # GIVEN
         table = Table(1,10,1,[])
-        message_attendu = "Indice de joueur incorrect"
+        message_attendu = f"Indice plus grand que le nombre de joueurs : {len(table.joueurs)}"
         indice = 2
 
         # WHEN / THEN
         with pytest.raises(IndexError, match=message_attendu):
             table.retirer_joueur(indice)
 
+    def test_table_retirer_joueur_valeur_incorrecte3(self):
+        # GIVEN
+        table = Table(1,10,1,[])
+        message_attendu = "L'indice doit être un entier"
+        indice = "a"
+
+        # WHEN / THEN
+        with pytest.raises(TypeError, match=message_attendu):
+            table.retirer_joueur(indice)
+
     def test_table_mettre_grosse_blind_succes(self):
         # GIVEN
         joueur1 = Joueur(1, "alice", 1000, True, "France", 20)
-        joueur2 = Joueur(2, "bernard", 500, True, "France", 25)
-        table = Table(10,10,1,[joueur1, joueur2])
-        credit = joueur2.credit
-        credit_attendu = credit - 10
+        table = Table(10,10,1,[joueur1])
+        credit = 50
         
         # WHEN
         table.mettre_grosse_blind(credit)
 
         # THEN
-        assert True == True
-        assert joueur2.credit == credit_attendu
+        assert table.grosse_blind == credit
+    
+    def test_table_mettre_grosse_blind_valeur_incorrecte3(self):
+        # GIVEN
+        table = Table(1,10,1,[])
+        message_attendu = "Le crédit doit être un entier"
+        credit = "a"
+
+        # WHEN / THEN
+        with pytest.raises(TypeError, match=message_attendu):
+            table.mettre_grosse_blind(credit)
 
     def test_table_rotation_dealer_succes(self):
         # GIVEN
