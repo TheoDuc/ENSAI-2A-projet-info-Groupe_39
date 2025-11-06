629314e7c0470f70e0eb86473207287fb23d9647
marinepac123 <marinepachy@gmail.com>
2025-11-06 09:10:56 +0000

    test joueur dao

--- data/pop_db.sql
+++ data/pop_db.sql
@@ -4,13 +4,12 @@ INSERT INTO manche(carte1, carte2, carte3, carte4, carte5) VALUES
 ('3 de Pique', 'Dame de Pique', '8 de Carreau', '9 de Pique','10 de Pique');
 
 INSERT INTO joueur(pseudo, credit, pays) VALUES
-('admin',       0,       'fr'),
-('a',           20,      'fr'),
-('maurice',     20,      'fr'),
-('batricia',    25,      'fr'),
-('miguel',      23,      'us'),
-('gilbert',     21,      'uk'),
-('junior',      15,      'fr');
+('admin',       0,         'fr'),
+('théo',        1000,      'fr'),
+('nil',         2000,      'fr'),
+('cheick',      2500,      'fr'),
+('guillaume',   2300,      'us'),
+('marine',      2100,      'uk');
 
 INSERT INTO manche_joueur(id_joueur, id_manche, carte_main_1, carte_main_2, mise, gain, tour_couche) VALUES
 (998, 998, '3 de Pique', 'Dame de Pique', 100, -100, 3),
--- src/tests/tests_dao/test_joueur_dao.py
+++ src/tests/tests_dao/test_joueur_dao.py
@@ -1,28 +1,147 @@
-import unittest
-from unittest.mock import patch
+import pytest
 
+from utils.reset_database import ResetDatabase
 from business_object.joueur import Joueur
-from service.joueur_service import JoueurService
+from dao.joueur_dao import JoueurDao
+
+class TestJoueurDao():
+
+    def test_creer_succes(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            pseudo = 'paul'
+            pays = 'fr'
+            joueurDao = JoueurDao()
+
+            # WHEN
+            resultat = joueurDao.creer(pseudo, pays)
+
+            # THEN
+            assert resultat == True
+
+    def test_creer_echec(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            pseudo = 'paul'
+            pays = 'fr'
+            joueurDao = JoueurDao()
+
+            # WHEN
+            resultat1 = joueurDao.creer(pseudo, pays)
+            resultat2 = joueurDao.creer(pseudo, pays)
+
+            # THEN
+            assert resultat1 == True
+            assert resultat2 == False
+
+    def test_trouver_par_id_succes(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            id_joueur = 999
+            joueur_recherche = Joueur(999,'admin', 0, 'fr')
+
+            # WHEN
+            resultat = joueurDao.trouver_par_id(id_joueur)
+
+            # THEN
+            assert resultat==joueur_recherche
+
+    def test_trouver_par_id_echec(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            id_joueur = 1
+            joueur_recherche = Joueur(999,'admin', 0, 'fr')
+
+            # WHEN
+            resultat = joueurDao.trouver_par_id(id_joueur)
+
+            # THEN
+            assert resultat==None
+
+    def test_lister_tous(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            liste = [
+                Joueur(999, 'admin', 0, 'fr'),
+                Joueur(998, 'a', 20, 'us'),
+                Joueur(997, 'maurice', 20, 'uk'),
+                Joueur(996, 'batricia', 25, 'fr')
+                ]
+
+            # WHEN
+            resultat = joueurDao.lister_tous()
+
+            # THEN
+            assert resultat==liste
 
 
-class TestJoueurService(unittest.TestCase):
-    def setUp(self):
-        self.service = JoueurService()
+    def test_modifier_succes(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            joueur2 = Joueur(999, 'admin', 50, 'fr')
 
-    @patch("dao.joueur_dao.JoueurDao.creer")
-    def test_creer_joueur_reussi(self, mock_creer):
-        mock_creer.return_value = True
-        joueur = self.service.creer("Alice", credit=1000, pays="France")
-        self.assertIsNotNone(joueur)
-        self.assertEqual(joueur.pseudo, "Alice")
-        mock_creer.assert_called_once()
+            # WHEN
+            resultat = joueurDao.modifier(joueur2)
 
-    @patch("dao.joueur_dao.JoueurDao.lister_tous")
-    def test_lister_tous(self, mock_lister):
-        mock_lister.return_value = [
-            Joueur(1, "Alice", 1000, "France"),
-            Joueur(2, "Bob", 500, "USA"),
-        ]
-        joueurs = self.service.lister_tous()
-        self.assertEqual(len(joueurs), 2)
-        self.assertEqual(joueurs[0].pseudo, "Alice")
+            # THEN
+            assert resultat==True
+            assert joueurDao.trouver_par_id(999)==joueur2
+
+    def test_modifier_echec(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            joueur2 = Joueur(1, 'admin', 50, 'fr')
+
+            # WHEN
+            resultat = joueurDao.modifier(joueur2)
+
+            # THEN
+            assert resultat==False
+            assert joueurDao.trouver_par_id(999)!=joueur2
+
+    def test_supprimer_succes(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            joueur = Joueur(999, 'admin', 50, 'fr')
+
+            # WHEN
+            resultat = joueurDao.supprimer(joueur)
+
+            # THEN
+            assert resultat==True
+            assert joueurDao.trouver_par_id(999)==None
+
+    def test_supprimer_echec(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            joueur = Joueur(1, 'admin', 50, 'fr')
+
+            # WHEN
+            resultat = joueurDao.supprimer(joueur)
+
+            # THEN
+            assert resultat==False
+            assert joueurDao.trouver_par_id(1)==None
+
+    def test_connection_succes(self):
+        if ResetDatabase().lancer(True):
+            # GIVEN
+            joueurDao = JoueurDao()
+            joueur = Joueur(999, 'admin', 50, 'fr')
+            pseudo = 'admin'
+
+            # WHEN
+            resultat = joueurDao.se_connecter(pseudo)
+
+            # THEN
+            assert resultat==joueur
+            
+
+
