50051fb3d6c51810d5ea65792d8f7fb715b5eef2
marinepac123 <marinepachy@gmail.com>
2025-11-15 16:40:44 +0000

    app.py debut avec encore quelques probleme

--- src/essai.py
+++ src/essai.py
@@ -4,10 +4,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
-from business_object.joueur import Joueur
 from service.joueur_service import JoueurService
-from service.credit_service import CreditService
-from utils.reset_database import ResetDatabase
 from utils.log_init import initialiser_logs
 
 app = FastAPI(title="Mon webservice")
@@ -16,7 +13,6 @@ app = FastAPI(title="Mon webservice")
 initialiser_logs("Webservice")
 
 joueur_service = JoueurService()
-credit_service = CreditService()
 
 
 @app.get("/", include_in_schema=False)
@@ -24,57 +20,41 @@ async def redirect_to_docs():
     """Redirect to the API documentation"""
     return RedirectResponse(url="/docs")
 
-# exemple pour plus tard
-@app.put("/admin/crediter2/{joueur}/{montant}", tags=["Admin"])
-def crediter(joueur, montant:int):
+@app.get("/admin/crediter/{joueur}/{montant}")
+def crediter(joueur: str, montant:int):
     reponse = input("Etes-vous un admin (oui ou non)")
     if reponse == 'oui':
-        credit_service.crediter(joueur,montant)
+        CreditService().crediter(joueur,montant)
     else:
         return (f"vous n'êtes pas admin")
     return(f"l'admin a bien crediter {montant} à {joueur}")
 
-# fonctionne pas car Joueur() devient un str en entree
-@app.put("/admin/crediter/{joueur}/{montant}", tags=["Admin"])
-def crediter(joueur, montant:int):
-    credit_service.crediter(joueur,montant)
+@app.get("/admin/debiter/{joueur}/{montant}")
+def debiter(joueur: str, montant:int):
+    reponse = input("Etes-vous un admin (oui ou non)")
+    if reponse == 'oui':
+        CreditService().debiter(joueur,montant)
+    else:
+        return (f"vous n'êtes pas admin")
     return(f"l'admin a bien crediter {montant} à {joueur}")
 
-# fonctionne pas car Joueur() devient un str en entree
-@app.put("/admin/debiter/{joueur}/{montant}", tags=["Admin"])
-def debiter(joueur, montant:int):
-    credit_service.debiter(joueur,montant)
-    return(f"l'admin a bien debiter {montant} à {joueur}")
-
-# fonctionne 
-@app.get("/joueur/liste", tags=["Joueurs"])
-async def joueur_lister():
-    """Liste tous les joueurs"""
-    logging.info("Liste tous les joueurs")
-    return joueur_service.lister_tous()
-
-# fonctionne 
-@app.get("/joueur/id/{id_joueur}", tags=["Joueurs"])
+    
+@app.get("/joueur/{id_joueur}", tags=["Joueurs"])
 async def joueur_par_id(id_joueur: int):
     """Trouver un joueur à partir de son id"""
     logging.info("Trouver un joueur à partir de son id")
     return joueur_service.trouver_par_id(id_joueur)
 
-# fonctionne 
-@app.get("/joueur/{pseudo}", tags=["Joueurs"])
-async def joueur_par_pseudo(pseudo: str):
-    """Trouver un joueur à partir de son pseudo"""
-    logging.info("Trouver un joueur à partir de son pseudo")
-    return joueur_service.trouver_par_pseudo(pseudo)
 
 class JoueurModel(BaseModel):
     """Définir un modèle Pydantic pour les Joueurs"""
 
     id_joueur: int | None = None  # Champ optionnel
     pseudo: str
+    credit: int
     pays: str
 
-# fonctionne 
+
 @app.post("/joueur/", tags=["Joueurs"])
 async def creer_joueur(j: JoueurModel):
     """Créer un joueur"""
@@ -82,13 +62,13 @@ async def creer_joueur(j: JoueurModel):
     if joueur_service.pseudo_deja_utilise(j.pseudo):
         raise HTTPException(status_code=404, detail="Pseudo déjà utilisé")
 
-    joueur = joueur_service.creer(j.pseudo, j.pays)
+    joueur = joueur_service.creer(j.pseudo, j.mdp, j.age, j.mail, j.fan_pokemon)
     if not joueur:
         raise HTTPException(status_code=404, detail="Erreur lors de la création du joueur")
 
     return joueur
 
-# probleme avec les joueur.pays et joueur.pays qu'on ne peut pas modifier
+
 @app.put("/joueur/{id_joueur}", tags=["Joueurs"])
 def modifier_joueur(id_joueur: int, j: JoueurModel):
     """Modifier un joueur"""
@@ -98,19 +78,22 @@ def modifier_joueur(id_joueur: int, j: JoueurModel):
         raise HTTPException(status_code=404, detail="Joueur non trouvé")
 
     joueur.pseudo = j.pseudo
-    joueur.pays = j.pays
+    joueur.mdp = j.mdp
+    joueur.age = j.age
+    joueur.mail = j.mail
+    joueur.fan_pokemon = j.fan_pokemon
     joueur = joueur_service.modifier(joueur)
     if not joueur:
         raise HTTPException(status_code=404, detail="Erreur lors de la modification du joueur")
 
     return f"Joueur {j.pseudo} modifié"
 
-# fonctionne 
-@app.delete("/joueur/{pseudo}", tags=["Joueurs"])
-def supprimer_joueur(pseudo: str):
+
+@app.delete("/joueur/{id_joueur}", tags=["Joueurs"])
+def supprimer_joueur(id_joueur: int):
     """Supprimer un joueur"""
     logging.info("Supprimer un joueur")
-    joueur = joueur_service.trouver_par_pseudo(pseudo)
+    joueur = joueur_service.trouver_par_id(id_joueur)
     if not joueur:
         raise HTTPException(status_code=404, detail="Joueur non trouvé")
 
@@ -118,10 +101,15 @@ def supprimer_joueur(pseudo: str):
     return f"Joueur {joueur.pseudo} supprimé"
 
 
+@app.get("/hello/{name}")
+async def hello_name(name: str):
+    """Afficher Hello"""
+    logging.info("Afficher Hello")
+    return f"message : Hello {name}"
+
+
 # Run the FastAPI application
 if __name__ == "__main__":
-    ResetDatabase().lancer()
-
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=5432)
