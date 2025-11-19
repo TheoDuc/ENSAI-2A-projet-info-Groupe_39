addf763ca57bf6ad472bd6166b9fd3db72fdc96e
YATABARE <yatabarecheikamala@gmail.com>
2025-11-19 20:41:11 +0100

    modif session

--- src/view/session.py
+++ src/view/session.py
@@ -36,13 +36,25 @@ class Session(metaclass=Singleton):
         self.debut_connexion = None
 
     def afficher(self) -> str:
-        """Afficher les informations de connexion"""
+        """Afficher les informations de connexion et les joueurs à la table"""
         res = "Actuellement en session :\n"
         res += "-------------------------\n"
+
+        # Joueurs connectés
         for j in Session.joueurs_connectes:
-            res += f"joueur : {j.pseudo} : {j.credit} crédits\n"
+            res += f"joueur connecté : {j.pseudo} : {j.credit} crédits\n"
             debut = getattr(j, "debut_connexion", None)
             if debut:
                 res += f"debut_connexion : {debut}\n"
             res += "\n"
+
+        # Joueurs à la table du joueur actuel
+        joueur = self.joueur
+        if joueur and joueur.table:
+            res += f"Joueurs à la table {joueur.table.numero_table} :\n"
+            res += "-------------------------\n"
+            for j in joueur.table.joueurs:
+                res += f"{j.pseudo} : {j.credit} crédits\n"
+            res += "\n"
+
         return res
