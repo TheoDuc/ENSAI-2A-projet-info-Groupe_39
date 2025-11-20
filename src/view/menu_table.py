29e936e2d7f47de0700065ade113bac494d36192
YATABARE <yatabarecheikamala@gmail.com>
2025-11-20 10:23:49 +0100

    mise ajour

--- src/view/menu_table.py
+++ src/view/menu_table.py
@@ -39,7 +39,27 @@ class MenuTable(VueAbstraite):
         if choix in boutons_tables:
             numero_table = int(choix[6])
             pseudo = Session().joueur.pseudo
+
             req = requests.put(f"{host}{END_POINT}ajouter/{numero_table}/{pseudo}")
+            if req.status_code != 200:
+                from view.menu_joueur_vue import MenuJoueurVue
+
+                return MenuJoueurVue()
+            table_req = requests.get(f"{host}{END_POINT}{numero_table}")
+            table_data = table_req.json()
+            if numero_table not in Session.tables_globales:
+                from business_object.joueur import Joueur
+                from business_object.table import Table
+
+                Session.tables_globales[numero_table] = Table(
+                    numero_table=table_data["numero_table"],
+                    grosse_blind=table_data["grosse_blind"],
+                    joueurs=[
+                        Joueur(j["id_joueur"], j["pseudo"], j["credit"], j["pays"])
+                        for j in table_data["joueurs"]
+                    ],
+                )
+            Session().joueur.table = Session.tables_globales[numero_table]
 
             if req.status_code != 200:
                 print("La connexion à la table n'a pas fonctionné")
