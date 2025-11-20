468b1f2e09f97722ce9fb0555b1a09358e2f1333
marinepac123 <marinepachy@gmail.com>
2025-11-20 15:03:12 +0000

    fin correction vue

--- src/view/menu_manche.py
+++ src/view/menu_manche.py
@@ -5,6 +5,7 @@ import requests
 from InquirerPy import inquirer
 
 from view.session import Session
+from service.joueur_service import JoueurService
 from view.vue_abstraite import VueAbstraite
 
 logger = logging.getLogger(__name__)
@@ -42,45 +43,52 @@ class MenuManche(VueAbstraite):
 
         match choix:
             case "Voir les infos de la manche":
-                numero_table = 1
-                # numero_table = Session().joueur.table.numero_table
+                # numero_table = 1
+                joueur = JoueurService().trouver_par_id(Session().id)
+                numero_table = joueur.table.numero_table
                 req = requests.get(f"{host}/table/affichage/{numero_table}")
                 if req.status_code == 200:
                     print(req.text)
                 return MenuManche()
 
             case "Regarder main":
-                numero_table = 1
-                id_joueur =  Session().joueur.id_joueur
-                # numero_table = Session().joueur.table.numero_table
+                # numero_table = 1
+                joueur = JoueurService().trouver_par_id(Session().id)
+                id_joueur =  Session().id
+                numero_table = joueur.table.numero_table
                 req = requests.get(f"{host}/table/main/{numero_table}/{id_joueur}")
                 if req.status_code == 200:
                     print(req.text)
                 return MenuManche()
 
             case "Checker":
-                pseudo = Session().joueur.pseudo
+                joueur = JoueurService().trouver_par_id(Session().id)
+                pseudo = joueur.pseudo
                 req = requests.put(f"{host}{END_POINT}checker/{pseudo}")
                 return MenuManche()
 
             case "Suivre":
-                pseudo = Session().joueur.pseudo
+                joueur = JoueurService().trouver_par_id(Session().id)
+                pseudo = joueur.pseudo
                 req = requests.put(f"{host}{END_POINT}suivre/{pseudo}")
                 return MenuManche()
 
             case "All in":
-                pseudo = Session().joueur.pseudo
+                joueur = JoueurService().trouver_par_id(Session().id)
+                pseudo = joueur.pseudo
                 req = requests.put(f"{host}{END_POINT}all_in/{pseudo}")
                 return MenuManche()
 
             case "Se coucher":
-                pseudo = Session().joueur.pseudo
+                joueur = JoueurService().trouver_par_id(Session().id)
+                pseudo = joueur.pseudo
                 req = requests.put(f"{host}{END_POINT}se_coucher/{pseudo}")
                 return MenuManche()
 
             case "Quitter manche":
-                # numero_table = Session().joueur.table.numero_table
-                numero_table = 1
+                joueur = JoueurService().trouver_par_id(Session().id)
+                numero_table = joueur.table.numero_table
+                # numero_table = 1
                 req = requests.get(f"{host}/table/terminer/{numero_table}")
 
                 if req.status_code == 200:
