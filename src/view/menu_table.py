17105414b6cb845fd7ecad36fb2ef00738e61453
marinepac123 <marinepachy@gmail.com>
2025-11-20 13:58:11 +0000

    vue table et lancement manche

--- src/view/menu_table.py
+++ src/view/menu_table.py
@@ -49,7 +49,7 @@ class MenuTable(VueAbstraite):
                 from view.menu_joueur_vue import MenuJoueurVue
 
                 return MenuJoueurVue()
-            from view.game_menu_view import GameMenu
+            from view.menu_lancement_manche import GameMenu
 
             print(f"Vous êtes connecté sur la table {numero_table}")
             return GameMenu()
@@ -79,7 +79,7 @@ class MenuTable(VueAbstraite):
             if Session().joueur not in Session.joueurs_connectes:
                 Session.joueurs_connectes.append(Session().joueur)
 
-            from view.game_menu_view import GameMenu
+            from view.menu_lancement_manche import GameMenu
 
             print(f"Vous êtes connecté sur la table {numero_table}")
             return GameMenu()
